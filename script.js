c108aa7025ad13d4df951ffc4f808ca6ae5f7d1d
Daniela Kůsová <danielka.kusova@gmail.com>
2020-10-22 19:31:31 +0200

    Co s tím

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ let currentPlayerImg = document.querySelector('#currentPlayerImg');
 
 gameArea.addEventListener('click', (event) => {
   if (currentPlayer === 'circle') {
-    event.target.setAttribute('disabled', true);
+    event.target.disabled = true;
     event.target.className = 'board__field--circle';
     currentPlayer = 'cross';
     currentPlayerImg.src = 'cross.svg';
   } else if (currentPlayer === 'cross') {
-    event.target.setAttribute('disabled', true);
+    event.target.disabled = true;
     event.target.className = 'board__field--cross';
     currentPlayer = 'circle';
     currentPlayerImg.src = 'circle.svg';
