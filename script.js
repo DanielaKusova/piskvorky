17dabb1f5a77ce7438714a172e65f0815e8433cb
Daniela Kůsová <danielka.kusova@gmail.com>
2020-10-22 17:51:08 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const button = document.querySelector('.butt-hra');
+
+let currentPlayer = 'circle';
+const gameArea = document.querySelector('.pole');
+let currentPlayerImg = document.querySelector('#currentPlayerImg');
+
+gameArea.addEventListener('click', (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.setAttribute('disabled', true);
+    event.target.className = 'board__field--circle';
+    currentPlayer = 'cross';
+    currentPlayerImg.src = 'cross.svg';
+  } else if (currentPlayer === 'cross') {
+    event.target.setAttribute('disabled', true);
+    event.target.className = 'board__field--cross';
+    currentPlayer = 'circle';
+    currentPlayerImg.src = 'circle.svg';
+  }
+});
